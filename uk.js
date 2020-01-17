a954c061f5740ed4b17ed44336d7c45285159167
Alexander <a.kabanov.pz@gmail.com>
2020-01-17 17:50:31 +0200

    Added basic uk lacale

---
+++ uk.js
@@ -0,0 +1,64 @@
+l10n.translations.uk = {
+    logout: 'Вийти',
+    error: {
+        a: 'Щось пішло не так. Будь ласка',
+        b: 'перезавантажити',
+        c: 'Технічні подробиці:'
+    },
+    login: {
+        title: 'Завантажити файли з %source',
+        lineA: {
+            instagram: 'Виберіть фото з вашого акаунту.'
+        },
+        lineB: 'Ми про все подбаємо.<br> Вам достатньо лише увійти до системи.',
+        note: 'Сторінка входу відкриється у новому вікні.',
+    },
+    pagination: {
+        loading: 'Завантаження...',
+        loadMore: 'Завантажити ще',
+        failed: {
+            a: 'Не вдалося завантажити.',
+            b: 'Спробувати ще'
+        }
+    },
+    instagram: {
+        menu: {
+            myPhotos: 'Мої фото',
+            myLikes: 'Мені подобається',
+            myFollowings: 'Мої підписки',
+            search: 'Пошук'
+        },
+        search: {
+            placeholder: 'Шукати фотографії за хештегом',
+            button: 'Пошук'
+        },
+        errors: {
+            account_is_private: 'Це приватний акаунт'
+        }
+    },
+    // pluralization example
+    files: {
+        one: '%n файл',
+        few: '%n файли',
+        many: '%n файлів'
+    },
+    list: {
+        table: {
+            title: 'Назва',
+            size: 'Розмір'
+        }
+    }
+};
+
+// Pluralization rules taken from:
+// http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
+l10n.pluralize.uk = function (n) {
+    return ((n/10%10|0) == 1) || (n%10 == 0) || (n%10 > 4) ?
+    'many' : ((n%10 == 1) ? 'one' : 'few');
+};
+
+l10n.date.uk = function(d) {
+    var months = ['Січ', 'Лют', 'Бер', 'Квіт', 'Трав', 'Чер', 'Лип', 'Серп', 'Вер', 'Жов', 'Лис', 'Груд'];
+
+    return "" + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+};
